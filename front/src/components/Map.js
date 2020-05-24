1a2a284fc089ab7c6be1dd811dae43770ad9536f
Patrick <Patrick@wyhn.ca>
2020-05-23 22:31:29 -0400

    prep for cards

--- front/src/components/Map.js
+++ front/src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { distance, geoJSON, sortGeoJSON } from "../utils/mapUtil";
 
 const styles = {
   display: "flex",
@@ -23,6 +24,8 @@ const MapboxGLMap = () => {
         zoom: 10, // starting zoom
       });
 
+      let markers = [];
+      let position = {};
       // get current location
       map.addControl(
         new mapboxgl.GeolocateControl({
@@ -37,16 +40,17 @@ const MapboxGLMap = () => {
         map.resize();
 
         // create a HTML element for each feature
-        var el = document.createElement("div");
+        const el = document.createElement("div");
+
         el.className = "marker";
         //
-        let markers = [];
-        let position = {};
-        let getParks = async (lat, lng, dist) => {
-          let res = await fetch("http://localhost:5000/parks");
-          let geoJSON = await res.json();
 
-          let closest = { lat: 0, lng: 0, distance: 100 };
+        let getParks = async (lat, lng, dist) => {
+          let closest = {
+            lat: 0,
+            lng: 0,
+            distance: 100,
+          };
           geoJSON.forEach((el) => {
             let d = distance(
               position.lat,
@@ -74,18 +78,19 @@ const MapboxGLMap = () => {
         };
 
         let backupGeoLocate = async () => {
-          console.log("Backup oops");
           let res = await fetch("http://localhost:5000/geo");
           let data = await res.json();
           setPosition(data.latitude, data.longitude, 100);
         };
+
         let setPosition = (lat, lng, acc) => {
           position = { lat: lat, lng: lng };
-          //map.setCenter([lng, lat]);
+          map.setCenter([lng, lat]);
           //map.setZoom(zoom);
           markers.push(new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map));
           getParks();
         };
+
         if ("geolocation" in navigator) {
           navigator.geolocation.getCurrentPosition(
             (pos) => {
@@ -105,33 +110,7 @@ const MapboxGLMap = () => {
         } else {
           backupGeoLocate();
         }
-        // from: https://www.geodatasource.com/developers/javascript
-        let distance = (lat1, lon1, lat2, lon2, unit) => {
-          if (lat1 === lat2 && lon1 === lon2) {
-            return 0;
-          } else {
-            var radlat1 = (Math.PI * lat1) / 180;
-            var radlat2 = (Math.PI * lat2) / 180;
-            var theta = lon1 - lon2;
-            var radtheta = (Math.PI * theta) / 180;
-            var dist =
-              Math.sin(radlat1) * Math.sin(radlat2) +
-              Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            if (dist > 1) {
-              dist = 1;
-            }
-            dist = Math.acos(dist);
-            dist = (dist * 180) / Math.PI;
-            dist = dist * 60 * 1.1515;
-            if (unit === "K") {
-              dist = dist * 1.609344;
-            }
-            if (unit === "N") {
-              dist = dist * 0.8684;
-            }
-            return dist;
-          }
-        };
+
         //
         // make a marker for each feature and add to the map
       });
@@ -140,7 +119,14 @@ const MapboxGLMap = () => {
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
 
-  return <div ref={(el) => (mapContainer.current = el)} style={styles} />;
+  // from: https://www.geodatasource.com/developers/javascript
+  sortGeoJSON();
+  return (
+    <div>
+      <div ref={(el) => (mapContainer.current = el)} style={styles} />
+      <div></div>
+    </div>
+  );
 };
 
 export default MapboxGLMap;
---
+++ front/src/utils/mapUtil.js
@@ -0,0 +1,63 @@
+const fetchParks = async () => {
+  return await fetch("http://localhost:5000/parks");
+};
+
+export const geoJSON = fetchParks();
+export const distance = (lat1, lon1, lat2, lon2, unit) => {
+  if (lat1 === lat2 && lon1 === lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit === "K") {
+      dist = dist * 1.609344;
+    }
+    if (unit === "N") {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
+};
+
+export const sortGeoJSON = () => {
+  addDistance(geoJSON);
+  const arr = Object.keys(geoJSON).forEach((key) => [key, geoJSON[key]]);
+  console.log(arr);
+  return arr;
+};
+
+const addDistance = async (...json) => {
+  let position = getLocation();
+  json.forEach(async (el) => {
+    //
+    console.log(await el);
+    let d = distance(
+      position.lat,
+      position.lng,
+
+      el.geometry.coordinates[1],
+      el.geometry.coordinates[0],
+      "K"
+    );
+
+    //
+    el.properties.distance = d;
+  });
+};
+
+const getLocation = async () => {
+  let res = await fetch("http://localhost:5000/geo");
+  let data = await res.json();
+  return data;
+};
