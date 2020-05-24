69961fa236b889ec907610ce22a481fa8ca971c0
Patrick <Patrick@wyhn.ca>
2020-05-24 15:54:31 -0400

    Moved app.js into pages cause its homepage

--- front/src/components/pages/App.js
+++ front/src/components/pages/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import { useAuth0 } from "../../react-auth0-spa";
-import history from "../../history";
-import Navigation from "../Navigation";
-import Parallax from "../Parallax";
-import Feed from "../Feed";
+import { useAuth0 } from "../react-auth0-spa";
+import history from "../history";
+import Navigation from "./Navigation";
+import Parallax from "./Parallax";
+import Feed from "./Feed";
+import Map from "./Map";
+
 import Contact from "./Contact";
 import About from "./About";
-import Park from "./Park";
-import Loading from "../Loading";
+import Loading from "./Loading";
 import "materialize-css";
-import LogoParkWalk from "../../components/logo.svg";
+import LogoParkWalk from "../components/logo.svg";
 
 class App extends Component {
   constructor(props) {
@@ -24,10 +25,11 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const url = "http://localhost:5000/parks";
+    const url = "http://localhost:5000/parksOttawa";
     const response = await axios.get(url);
     return this.setState({ feeds: response.data });
   }
+
   render() {
     const { loading } = useAuth0;
 
@@ -37,33 +39,38 @@ class App extends Component {
     return (
       <React.StrictMode>
         <Router history={history}>
-          <div className="grey-text text-darken-2">
-            <nav className="white" role="navigation">
-              <Navigation />
-            </nav>
-            <div className="container">
-              <div className="row center">
-                <img
-                  src={LogoParkWalk}
-                  className="brand-logo black-text"
-                  width="300
+          <nav className="white" role="navigation">
+            <Navigation />
+          </nav>
+          <div className="container">
+            <div className="row center">
+              <img
+                src={LogoParkWalk}
+                className="brand-logo black-text"
+                width="300
                   px"
-                  alt="un lugar en Ottawa"
-                ></img>
-              </div>
+                alt="un lugar en Ottawa"
+              ></img>
+            </div>
 
-              <Parallax title={this.state.parallaxTitle} />
-              <Switch>
-                <Route path="/contact" component={Contact} />
-                <Route path="/about" component={About} />
-                <Route path="/park" component={Park} />
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => <Feed feeds={this.state.feeds} />}
-                />
-              </Switch>
-
+            <Switch>
+              <Route path="/map" component={Map} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/about" component={About} />
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <React.StrictMode>
+                    {" "}
+                    <Parallax title={this.state.parallaxTitle} />
+                    <Feed feeds={this.state.feeds} />
+                  </React.StrictMode>
+                )}
+              />
+              />
+            </Switch>
+            <div className="container">
               <div className="footer">
                 <p>&copy; {this.state.name}</p>
               </div>
